922a31ff7d3f896f35dbe05f390f6331926dbbe3
bensterone <157856243+bensterone@users.noreply.github.com>
2026-03-06 11:56:18 +0100

    Update Assets.php

--- includes/Frontend/Assets.php
+++ includes/Frontend/Assets.php
@@ -10,16 +10,27 @@ class Assets {
         if ( is_singular( FP360_CPT ) ) {
             wp_enqueue_style( 'fp360-viewer', FP360_URL . 'assets/css/viewer.css', [], FP360_VERSION );
             wp_enqueue_script( 'fp360-viewer', FP360_URL . 'assets/js/viewer.js', [], FP360_VERSION, true );
-            
+
             wp_localize_script( 'fp360-viewer', 'fp360Config', [
                 'ajaxUrl' => admin_url( 'admin-ajax.php' ),
-                'origin'  => window_get_origin(), // Helper for security
+                'origin'  => fp360_get_allowed_origin(),
             ] );
         }
     }
 }
 
-// Simple helper to get origin for JS
-function window_get_origin() {
-    return (is_ssl() ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'];
+function fp360_get_allowed_origin() {
+    $home = wp_parse_url( home_url( '/' ) );
+
+    if ( empty( $home['scheme'] ) || empty( $home['host'] ) ) {
+        return '';
+    }
+
+    $origin = $home['scheme'] . '://' . $home['host'];
+
+    if ( ! empty( $home['port'] ) ) {
+        $origin .= ':' . (int) $home['port'];
+    }
+
+    return esc_url_raw( $origin );
 }
