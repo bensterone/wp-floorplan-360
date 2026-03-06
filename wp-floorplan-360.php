d475bcefff8fd3801a9760ae6752154b271579cc
bensterone <157856243+bensterone@users.noreply.github.com>
2026-03-06 10:51:51 +0100

    Implement activation hook for post type registration
    Add activation hook to register post type and flush permalinks.

--- wp-floorplan-360.php
+++ wp-floorplan-360.php
@@ -28,3 +28,12 @@ function init() {
     $plugin->run();
 }
 add_action( 'plugins_loaded', 'Floorplan360\\init' );
+
+register_activation_hook( __FILE__, function() {
+    // 1. Manually trigger the post type registration
+    $post_type = new Core\PostType();
+    $post_type->register_cpt(); // We call the registration function directly
+    
+    // 2. Clear the permalinks
+    flush_rewrite_rules();
+});
