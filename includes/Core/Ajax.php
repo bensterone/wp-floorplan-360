a88242d0317da282ba78d7c8bb6a18a9718fc6fd
bensterone <157856243+bensterone@users.noreply.github.com>
2026-03-06 12:04:40 +0100

    Update Ajax.php

--- includes/Core/Ajax.php
+++ includes/Core/Ajax.php
@@ -13,21 +13,19 @@ class Ajax {
         header( 'Content-Security-Policy: frame-ancestors \'self\'' );
         header( 'Content-Type: text/html; charset=utf-8' );
 
-        $img   = isset( $_GET['img'] ) ? esc_url_raw( wp_unslash( $_GET['img'] ) ) : '';
-        // Now using the 'title' parameter if you want to display it inside the iframe
-        $title = isset( $_GET['title'] ) ? sanitize_text_field( wp_unslash( $_GET['title'] ) ) : '';
-        
+        $img = isset( $_GET['img'] ) ? esc_url_raw( wp_unslash( $_GET['img'] ) ) : '';
+
         if ( empty( $img ) ) {
             wp_die( 'No image specified.', 400 );
         }
 
-        $site_url  = wp_parse_url( get_site_url(), PHP_URL_HOST );
-        $image_url = wp_parse_url( $img, PHP_URL_HOST );
+        $allowed_host = wp_parse_url( home_url( '/' ), PHP_URL_HOST );
+        $image_host   = wp_parse_url( $img, PHP_URL_HOST );
 
-        if ( $image_url && $image_url !== $site_url ) {
+        if ( $image_host && $allowed_host && strtolower( $image_host ) !== strtolower( $allowed_host ) ) {
             wp_die( 'Unauthorized image source.', 403 );
         }
-        
+
         if ( file_exists( FP360_PATH . 'views/iframe-viewer.php' ) ) {
             require FP360_PATH . 'views/iframe-viewer.php';
         }
